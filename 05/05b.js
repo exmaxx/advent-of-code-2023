3bb4ac8949ff5c5e616418b98d5a85bf5e373d25
Marek Ulicny <xulicny@gmail.com>
2023-12-08 21:01:03 +0100

    day 5 wip

--- 05/05b.js
+++ 05/05b.js
@@ -1,7 +1,7 @@
 import { readFromFile, writeToFile } from '../utils.js'
 
 // Cheatsheet:
-// seed-to-soil map:
+// seed-to-soil ruleset:
 // 50 98 2
 // 52 50 48
 // destination source length
@@ -20,25 +20,25 @@ function readSeeds(lines) {
 }
 
 /**
- * Reads map groups from an array of lines.
+ * Reads rule sets from an array of lines.
  *
- * Each map group consists of maps that represented by a set of numbers. Each map is on
- * a single line. A map consist of three numbers - destination, source and length.
+ * Each rule set consists of rules that represented by a set of numbers. Each rule is on
+ * a single line. A rule consist of three numbers - destination, source and length.
  *
- * @param {string[]} lines - An array of lines representing the map groups.
- * @returns {number[][][]} - An array of map groups, where each map group is an array of arrays of numbers.
+ * @param {string[]} lines - An array of lines representing the rule sets.
+ * @returns {number[][][]} - An array of rule sets, where each rule set is an array of arrays of numbers.
  */
-function readMapGroups(lines) {
-  let maps = []
-  let mapGroup = []
+function readRuleSets(lines) {
+  let ruleSets = []
+  let ruleSet = []
 
   for (let i = 2; i < lines.length; i++) {
     const line = lines[i]
 
     // group ends
     if (line === '') {
-      maps.push(mapGroup)
-      mapGroup = []
+      ruleSets.push(ruleSet)
+      ruleSet = []
 
       continue
     }
@@ -46,14 +46,14 @@ function readMapGroups(lines) {
     const values = line.match(/\d+/g)?.map((match) => Number(match))
 
     if (values) {
-      mapGroup.push(values)
+      ruleSet.push(values)
     }
   }
 
   // last group
-  maps.push(mapGroup)
+  ruleSets.push(ruleSet)
 
-  return maps
+  return ruleSets
 }
 
 /**
@@ -77,13 +77,184 @@ function createIntervals(seeds) {
 }
 
 /**
- * Sorts a map group by the source ID.
+ * Sorts a rule set by the source ID.
  *
- * @param {number[][]} mapGroup - The map group to be sorted.
- * @returns {number[][]} - The sorted map group.
+ * @param {number[][]} ruleSet - The rule set to be sorted.
+ * @returns {number[][]} - The sorted rule set.
  */
-function sortMapGroupBySourceId(mapGroup) {
-  return mapGroup.toSorted((a, z) => a[1] - z[1])
+function sortRuleSetBySourceId(ruleSet) {
+  return ruleSet.toSorted((a, z) => a[1] - z[1])
+}
+
+/**
+ * Determines which part of rule interval overlaps the values interval.
+ *
+ * @param {[number[]]} valuesInterval - The values interval (serves as a reference).
+ * @param {[number[]]} ruleInterval - The rule interval.
+ * @returns {string} - The intersection type ('whole', 'end', or 'none').
+ */
+function detectIntersection(valuesInterval, ruleInterval) {
+  const [valuesStart, valuesEnd] = valuesInterval
+  const [ruleStart, ruleEnd] = ruleInterval
+  //
+  // console.log(valuesInterval, ruleInterval)
+  //
+  // if (valuesStart <= ruleStart && ruleEnd <= valuesEnd) {
+  //   return 'whole'
+  // } else if (ruleStart <= valuesStart && ruleEnd < valuesEnd) {
+  //   return 'end'
+  // } else if (valuesStart < ruleStart && valuesEnd <= ruleEnd) {
+  //   return 'start'
+  // } else if (ruleStart <= valuesStart && valuesEnd <= ruleEnd) {
+  //   return 'around'
+  // } else {
+  //   return 'none'
+  // }
+
+  // if valuesstart <= valuesend -> return first one
+
+  // return start interval
+  // return middle
+  // return rest
+}
+
+/**
+ * Creates intersecting intervals based on the given interval and intersecting rules.
+ *
+ * @param {number[]} interval - The original interval represented by [start, end].
+ * @param {number[][]} intersectingRules - The array of intersecting rules represented by [destinationStart, sourceStart, length].
+ * @returns {Object[][]} - An array of intersecting intervals.
+ */
+function createIntersectingIntervals(interval, intersectingRules) {
+  const [start, end] = interval
+  const anchors = []
+
+  anchors.push({ num: start, type: 'orig' })
+  anchors.push({ num: end, type: 'orig' })
+
+  if (!intersectingRules.length) {
+    return [anchors]
+  }
+
+  // gather all unique numbers
+  for (const rule of intersectingRules) {
+    const [destinationStart, sourceStart, length] = rule
+    const sourceEnd = sourceStart + length - 1
+
+    const diff = destinationStart - sourceStart
+
+    anchors.push({ num: sourceStart, type: 'rule', diff })
+    anchors.push({ num: sourceEnd, type: 'rule', diff })
+  }
+
+  // sort them
+  const sorted = anchors.toSorted((a, z) => a.num - z.num)
+
+  // remove first and last if needed
+  if (sorted.at(0).num <= start) {
+    const ruleAnchor = sorted.shift()
+    const origAnchor = sorted.shift()
+
+    sorted.unshift({ ...ruleAnchor, num: origAnchor.num })
+  }
+
+  if (end <= sorted.at(-1).num) {
+    const ruleAnchor = sorted.pop()
+    const origAnchor = sorted.pop()
+
+    sorted.push({ ...ruleAnchor, num: origAnchor.num })
+  }
+
+  // turn them into intervals
+  const intervals = []
+
+  for (let i = 0; i < sorted.length - 1; i++) {
+    intervals.push([sorted[i], sorted[i + 1]]) // this way, there are overlaps, we will see if that matters
+  }
+
+  return intervals
+}
+
+function applyDiffOnIntersectedInterval(intersectingInterval) {
+  const [start, end] = intersectingInterval
+
+  return start.type === 'rule'
+    ? [start.num + start.diff, end.num + start.diff]
+    : [start.num, end.num]
+}
+
+function applyRuleSet(interval, ruleSet) {
+  const [start, end] = interval
+  // const intersectingIntervals = []
+  const intersectingRules = []
+
+  console.log('interval:', interval)
+
+  for (const rule of ruleSet) {
+    console.log('rule:', rule)
+    const [_, sourceStart, length] = rule
+    const sourceEnd = sourceStart + length - 1
+
+    if (sourceEnd < start) continue // rule numbers are before interval numbers
+    if (end < sourceStart) continue // rule numbers are after interval numbers
+
+    intersectingRules.push(rule)
+    console.log(' -> yes', sourceStart, sourceEnd)
+  }
+
+  const intersectingIntervals = createIntersectingIntervals(
+    interval,
+    intersectingRules
+  )
+
+  const mappedIntervals = intersectingIntervals.map(
+    applyDiffOnIntersectedInterval
+  )
+
+  return mappedIntervals
+}
+
+function applyRuleSets(seedInterval, ruleSetsSorted) {
+  let intermediateResults = [seedInterval]
+
+  for (const ruleSet of ruleSetsSorted) {
+    const another = []
+
+    for (const intermediateResult of intermediateResults) {
+      const results = applyRuleSet(intermediateResult, ruleSet)
+      another.push(...results)
+    }
+
+    intermediateResults = another
+  }
+  // applyRuleSet(seedInterval, ruleSet) // test only
+
+  console.log('intermediateResults:', intermediateResults)
+}
+
+/**
+ * Calculates the minimum seeds for given seed intervals and rule sets.
+ *
+ * @param {Array} seedIntervals - An array of seed intervals.
+ * @param {Array} ruleSets - An array of rule sets.
+ * @returns {Array} - An array containing the calculated minimum seeds.
+ */
+function calculateMinSeeds(seedIntervals, ruleSets) {
+  const ruleSetsSorted = ruleSets.map((ruleSet) =>
+    sortRuleSetBySourceId(ruleSet)
+  )
+
+  console.log('ruleSetsSorted:', ruleSetsSorted)
+
+  const mins = []
+
+  for (const seedInterval of seedIntervals) {
+    const min = applyRuleSets(seedInterval, ruleSetsSorted)
+
+    mins.push(min)
+  }
+
+  return mins
 }
 
 /**
@@ -94,17 +265,15 @@ function main() {
   const lines = readFromFile('05-example.txt')
 
   const seeds = readSeeds(lines)
-  const mapGroups = readMapGroups(lines)
-
-  const mapGroupsSorted = mapGroups.map((mapGroup) =>
-    sortMapGroupBySourceId(mapGroup)
-  )
-
-  console.log('mapGroupsSorted:', mapGroupsSorted)
-
+  const ruleSets = readRuleSets(lines)
   const seedIntervals = createIntervals(seeds)
 
   console.log('seedIntervals:', seedIntervals)
+
+  const mins = calculateMinSeeds(seedIntervals, ruleSets)
+  const min = Math.min(...mins)
+
+  console.log('min:', min)
 }
 
 // 🚀
